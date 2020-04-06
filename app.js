0766d98e488fd2398142370ed7e3fff847e5f6b2
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2020-04-07 01:05:43 +0200

    refactored app with the creation of controllers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
-const logger = require('./utils/logger')
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -20,24 +22,7 @@ mongoose
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs',blogsRouter)
 
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 module.exports = app
