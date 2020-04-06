b308af62506be447bb57226979c2a89852615dc3
JasperSrrzn <jasper.boris.sarrazin@gmail.com>
2020-04-06 23:41:31 +0200

    refactored model

--- index.js
+++ index.js
@@ -3,15 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = 'mongodb+srv://fullstack:oXnKceAgXUwbI7DO@cluster0-prz4a.mongodb.net/bloglist?retryWrites=true&w=majority'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
